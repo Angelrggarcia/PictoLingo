2bc268a024ab95944ebc6f0961eff72036e61708
Samantha <samanthasofiabg@gmail.com>
2023-11-08 15:18:14 -0600

    Cambio imagenes

--- App/app/src/main/java/com/example/pictolingo/screens/UsersScreen.kt
+++ App/app/src/main/java/com/example/pictolingo/screens/UsersScreen.kt
@@ -10,12 +10,19 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.font.FontFamily
@@ -25,6 +32,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.pictolingo.components.PictureCard
+import com.example.pictolingo.components.SegmentedButtonItem
+import com.example.pictolingo.components.SegmentedButtons
 import com.example.pictolingo.components.TopBar
 import com.example.pictolingo.objects.getUsers
 
@@ -53,6 +62,8 @@ fun UsersScreen(navController: NavHostController) {
 
 @Composable
 fun UsersGrid(navController: NavHostController) {
+    val openAlertDialog = remember { mutableStateOf(false) }
+    var selectedIndex by remember { mutableStateOf(0) }
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         content = {
@@ -67,12 +78,43 @@ fun UsersGrid(navController: NavHostController) {
                     fontWeight = FontWeight.Bold,
                     fontFamily = FontFamily.SansSerif)
             }
+            item(span = {
+                GridItemSpan(maxLineSpan)
+            }){
+                SegmentedButtons (modifier = Modifier.padding(horizontal = 20.dp)) {
+                    SegmentedButtonItem(
+                        selected = selectedIndex == 0,
+                        onClick = { selectedIndex = 0 },
+                        label = { Text(text = "Alpha" )},
+                        icon = { Icon(imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Localized description",) }
+                    )
+                    SegmentedButtonItem(
+                        selected = selectedIndex == 1,
+                        onClick = { selectedIndex = 1 },
+                        label = { Text(text = "Bravo") },
+                        icon = { Icon(imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Localized description",) }
+                    )
+                    SegmentedButtonItem(
+                        selected = selectedIndex == 2,
+                        onClick = { selectedIndex = 2 },
+                        label = { Text(text = "Bravo") },
+                        icon = { Icon(imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Localized description",) }
+                    )
+                }
+            }
             items(items = getUsers()){ it ->
                 val enClick: () -> Unit = {navController.navigate("Pictograms")}
                 PictureCard(it.name, it.url, enClick)
             }
             item {
-                PictureCard(name = "Crea un nuevo usuario", imageURL = "", enClick = {})
+                PictureCard(
+                    name = "Crea un nuevo usuario",
+                    imageURL = "",
+                    enClick = { openAlertDialog.value = true }
+                )
             }
         },
         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 20.dp),
