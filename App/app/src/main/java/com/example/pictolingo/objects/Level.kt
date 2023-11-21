16e2b208dbe79024f2e4ef3d53e207d493d23f66
Samantha <samanthasofiabg@gmail.com>
2023-11-21 17:10:28 -0600

    Levels modification

--- App/app/src/main/java/com/example/pictolingo/objects/Level.kt
+++ App/app/src/main/java/com/example/pictolingo/objects/Level.kt
@@ -1,10 +1,14 @@
 package com.example.pictolingo.objects
 
 import androidx.compose.ui.graphics.Color
+import com.example.pictolingo.ui.theme.azul_verdoso
 import com.example.pictolingo.ui.theme.happy_face
+import com.example.pictolingo.ui.theme.morado
 import com.example.pictolingo.ui.theme.very_happy_face
 import com.example.pictolingo.ui.theme.neutral_face
+import com.example.pictolingo.ui.theme.rosa
 import com.example.pictolingo.ui.theme.sad_face
+import com.example.pictolingo.ui.theme.verde
 
 
 data class Level(
@@ -16,9 +20,9 @@ data class Level(
 
 fun getLevels(): List<Level> {
     return listOf(
-        Level("Fácil", 51, very_happy_face, Color(0xFF930000)),
-        Level("Medio", 42, happy_face, Color(0xFF930000)),
-        Level("Difícil", 35, neutral_face, Color(0xFF930000)),
-        Level("Muy difícil", 18, sad_face, Color(0xFF930000)),
+        Level("Fácil", 51, very_happy_face, azul_verdoso),
+        Level("Medio", 42, happy_face, verde),
+        Level("Difícil", 35, neutral_face, rosa),
+        Level("Muy difícil", 18, sad_face, morado),
     )
 }
