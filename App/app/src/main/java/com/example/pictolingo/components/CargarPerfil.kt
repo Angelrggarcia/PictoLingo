6c79cf1bfcb8b68fed6dafd356c5ae1afabcd855
Oskar Abraham Sandoval Garcia <osabga@gmail.com>
2023-11-29 00:57:00 -0600

    Added add profile api call

--- App/app/src/main/java/com/example/pictolingo/components/CargarPerfil.kt
+++ App/app/src/main/java/com/example/pictolingo/components/CargarPerfil.kt
@@ -1,5 +1,9 @@
 package com.example.pictolingo.components
 
+import android.content.ContentValues
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -27,6 +31,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
@@ -34,14 +39,62 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.pictolingo.ui.theme.hueso
 import com.example.pictolingo.ui.theme.md_theme_light_onPrimaryContainer
 import com.example.pictolingo.ui.theme.otro_blaco
+import org.json.JSONException
+import org.json.JSONObject
+
+
+fun cargarUsuario(context:Context, code:String, callback: (Boolean) -> Unit) {
+    val queue = Volley.newRequestQueue(context)
+    val url = "https://api-dev-crtb.1.us-1.fl0.io/api/user/add"
+
+    val jsonObject = JSONObject()
+    jsonObject.put("code", code)
+
+
+    val jsonObjectRequest = JsonObjectRequest(
+        url, jsonObject,
+        Response.Listener { response ->
+            Log.i(ContentValues.TAG, "Response is $response")
+
+            val message = try {
+                response.getString("message")
+            } catch (e: JSONException) {
+                Log.e(ContentValues.TAG, "JSONException: ${e.message}")
+                "Default Message"  // Default or error message
+            }
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+            callback(true)
+        },
+        Response.ErrorListener { error ->
+            error.printStackTrace()
+            Toast.makeText(context, "Hubo algun problema con su inicio de sesion", Toast.LENGTH_LONG).show()
+            callback(false)
+        }
+    )
+
+    queue.add(jsonObjectRequest)
+}
+
+
+
+
+
+
+
+
 
 // se encarga de generar la funcion para mandar codigos y traer el perfil
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CargarPerfil(navController : NavHostController){
+
+    val appContext = LocalContext.current.applicationContext
     var text by rememberSaveable { mutableStateOf("") }
 
     Column() {
@@ -108,7 +161,13 @@ fun CargarPerfil(navController : NavHostController){
                 )
                 Button(
                     onClick = {
-                        navController.navigate("AdminPictogramCat")
+                        cargarUsuario(appContext,text) { loginSuccessful ->
+                            if (loginSuccessful) {
+                                navController.navigate("AdminPictogramCat")
+                            } else {
+
+                            }
+                        }
                     },
                     modifier = Modifier
                         //.height(45.dp)
