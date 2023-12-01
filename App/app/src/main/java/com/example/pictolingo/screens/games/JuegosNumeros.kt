29f94a95e262ae217d3c263e32ec3b1743b1ca83
Samantha <samanthasofiabg@gmail.com>
2023-11-30 23:55:17 -0600

    Juego de numeros formato

--- App/app/src/main/java/com/example/pictolingo/screens/games/JuegosNumeros.kt
+++ App/app/src/main/java/com/example/pictolingo/screens/games/JuegosNumeros.kt
@@ -1,4 +1,5 @@
 package com.example.pictolingo.screens.games
+import android.os.Looper
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
@@ -14,19 +15,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.pictolingo.components.TopBar
-import com.example.pictolingo.screens.users.Levels
+import com.example.pictolingo.ui.theme.azul
+import com.example.pictolingo.ui.theme.blaco
 import com.example.pictolingo.ui.theme.hueso
 import com.example.pictolingo.ui.theme.otro_blaco
+import com.example.pictolingo.ui.theme.rosa_fuerte
+import com.example.pictolingo.ui.theme.verde
 import kotlin.random.Random
 
 
@@ -56,23 +60,51 @@ fun JuegosNumeros(navController: NavHostController){
 fun NumberGame() {
     var number1 by remember { mutableStateOf(generateRandomNumber()) }
     var number2 by remember { mutableStateOf(generateRandomNumber()) }
+    var color1 by remember { mutableStateOf(azul) }
+    var color2 by remember { mutableStateOf(azul) }
     var message by remember { mutableStateOf("") }
+
+    fun numberChecked(selected: Int) {
+        if(selected == 2) {
+            color2 = if (number2 >= number1) {
+                verde
+            } else {
+                rosa_fuerte
+            }
+        } else {
+            color1 = if (number2 <= number1) {
+                verde
+            } else {
+                rosa_fuerte
+            }
+        }
+
+        message = compareNumbers(number1, number2)
+
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            number1 = generateRandomNumber()
+            number2 = generateRandomNumber()
+            color1 = azul
+            color2 = azul
+        }, 2000)
+    }
     Box(
         modifier = Modifier
-            .fillMaxWidth(1f)
+            .fillMaxSize()
             .padding(8.dp)
             .background(
                 color = otro_blaco,
                 shape = RoundedCornerShape(40.dp))
             .border(width = 4.dp, color = hueso, shape = RoundedCornerShape(40.dp)),
 
-        contentAlignment = Alignment.TopCenter
+        contentAlignment = Alignment.Center,
     ){
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .align(Alignment.Center)
                 .padding(16.dp),
-            verticalArrangement = Arrangement.Top,
+            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
@@ -85,23 +117,30 @@ fun NumberGame() {
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Button(onClick = {
-                message = compareNumbers(number1, number2)
-                number1 = generateRandomNumber()
-                number2 = generateRandomNumber()
-            }) {
-                Text("Número 1: $number1")
+            Row {
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .padding(10.dp),
+                    colors = buttonColors(color1, blaco, color1, blaco),
+                    onClick = {
+                        numberChecked(1)
+                    }) {
+                    Text((number1.toString()), fontSize = 80.sp)
+                }
+
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    colors = buttonColors(color2, blaco, color2, blaco),
+                    onClick = {
+                        numberChecked(2)
+                    }) {
+                    Text((number2.toString()), fontSize = 80.sp)
+                }
             }
 
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Button(onClick = {
-                message = compareNumbers(number1, number2)
-                number1 = generateRandomNumber()
-                number2 = generateRandomNumber()
-            }) {
-                Text("Número 2: $number2")
-            }
 
             Spacer(modifier = Modifier.height(16.dp))
 
@@ -109,7 +148,6 @@ fun NumberGame() {
         }
     }
 }
-
 
 fun generateRandomNumber(): Int {
     return Random.nextInt(1, 30)
